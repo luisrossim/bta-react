e6a46abe0521acbce3d7501dc60e252ac691a5d8
Luis Gustavo Rossim <luisrossim23@gmail.com>
2025-07-28 23:48:43 -0300

    test: develop disclamer

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,14 @@ import { AuthProvider } from "./features/auth/contexts/AuthContext";
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-                <RouterProvider router={router} />
-            </AuthProvider>
+            <div className="relative">
+                <AuthProvider>
+                    <RouterProvider router={router} />
+                </AuthProvider>
+                <div className="absolute bottom-0 p-1 text-center text-xs md:text-sm w-full bg-amber-400">
+                    Esta é uma versão de teste, haverá carregamentos inicias longos e perca de dados
+                </div>
+            </div>
             <Toaster richColors />
         </QueryClientProvider>
     </StrictMode>
